4b620d62a32a23fa4e68181ff60378985a1317f3
Alex LaFroscia <alex@lafroscia.com>
2024-03-15 18:40:55 -0400

    start feed on button press

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
-const video = document.getElementById("video");
+const START_BUTTON = document.getElementById("start-button");
 
-// Automatically play once the video stream is ready
-video.addEventListener("loadeddata", () => {
-  video.play();
-});
+async function startVideoStream() {
+  const video = document.createElement("video");
 
-// Get access to the video camrea stream
-const mediaStream = await navigator.mediaDevices.getUserMedia({
-  audio: false,
-  video: {
-    aspectRatio: {
-      exact: 2 / 3,
+  const container = START_BUTTON.parentElement;
+
+  // Swap the `button` for the `video`
+  container.append(video);
+  container.removeChild(START_BUTTON);
+
+  // Automatically play once the video stream is ready
+  video.addEventListener("loadeddata", () => {
+    video.play();
+  });
+
+  // Get access to the video camrea stream
+  const mediaStream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      aspectRatio: {
+        exact: 2 / 3,
+      },
+      facingMode: "environment",
     },
-    facingMode: "environment",
-  },
-});
+  });
 
-// Attach it to the video element to view it
-video.srcObject = mediaStream;
+  // Attach it to the video element to view it
+  video.srcObject = mediaStream;
+}
+
+START_BUTTON.addEventListener("click", () => {
+  startVideoStream();
+});
